f716a7d716d81bb268d4a839eb9b05693c420619
Morganf13 <nikita-podhvatoff@yandex.ru>
2018-03-04 13:02:22 +0300

    Added generation starting numbers

--- helloworld.js
+++ helloworld.js
@@ -1,5 +1,6 @@
 var app;
 var buttons = new Array();
+var numbers = new Array();
 var firstBT, secondBT;
 
 const onClick = event =>{
@@ -26,17 +27,62 @@ const onClick = event =>{
         }
 }
 
+function addNumber(button){
+    if(button.value == 10)
+    {
+        var ten = new PIXI.Sprite() ;
+        var number = new PIXI.Sprite(numbers[1]);
+        number.scale.set(1.2);
+        number.anchor.set(0.5);
+        number.x = -10;
+        number.y = 3;
+        ten.addChild(number);
+        number = new PIXI.Sprite(numbers[0]);
+        number.anchor.set(0.5);
+        number.scale.set(1.2);
+        number.x = 10;
+        number.y = 0;
+        ten.addChild(number);
+        ten.anchor.set(0.5);
+        button.addChild(ten);
+    }
+    else
+    {
+        var number = new PIXI.Sprite(numbers[button.value]);
+        number.anchor.set(0.5);
+        number.scale.set(1.2);
+        button.addChild(number);
+    }
+};
+
 function addButton(){
     var button = new PIXI.Sprite.fromImage('chip.png');
     button.interactive = true;
     button.buttonMode = true;
     button.anchor.set(0.5);
+	button.value = Math.round(Math.random() * 10);
+	addNumber(button);                    
 	button.on('pointerdown', onClick);
     return button;
 };
 
+function initNumbers(){
+    var numberTex = PIXI.Texture.fromImage("/BetFont/SafeDigits-hd.png");
+    numbers.push(new PIXI.Texture(numberTex, new PIXI.Rectangle(0,57,20,20)));//0
+    numbers.push(new PIXI.Texture(numberTex, new PIXI.Rectangle(115,0,13,20)));//1
+    numbers.push(new PIXI.Texture(numberTex, new PIXI.Rectangle(0,39,20,18)));//2
+    numbers.push(new PIXI.Texture(numberTex, new PIXI.Rectangle(0,20,20,19)));//3
+    numbers.push(new PIXI.Texture(numberTex, new PIXI.Rectangle(0,0,20,20)));//4
+    numbers.push(new PIXI.Texture(numberTex, new PIXI.Rectangle(78,0,20,20)));//5
+    numbers.push(new PIXI.Texture(numberTex, new PIXI.Rectangle(59,00,20,20)));//6
+    numbers.push(new PIXI.Texture(numberTex, new PIXI.Rectangle(98,0,18,20)));//7
+    numbers.push(new PIXI.Texture(numberTex, new PIXI.Rectangle(40,0,20,20)));//8
+    numbers.push(new PIXI.Texture(numberTex, new PIXI.Rectangle(20,0,20,20)));//9
+};
+
 function init()
 {
+	initNumbers();
 	app = new PIXI.Application(window.innerWidth, 2000);
     app.renderer = PIXI.autoDetectRenderer(816, 816,{antialiasing: false, transparent: false, resolution: 1});
     document.body.appendChild(app.view);
