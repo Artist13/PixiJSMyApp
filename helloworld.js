2cff6ea2f037738150aca9290283df3837f67726
Morganf13 <nikita-podhvatoff@yandex.ru>
2018-03-04 14:39:44 +0300

    Added sound effects and new value for buttons getting from json

--- helloworld.js
+++ helloworld.js
@@ -1,8 +1,37 @@
+const log = console.log;
 var app;
-var buttons = new Array();
-var numbers = new Array();
 var firstBT, secondBT;
+var firstBG, secondBG;
+var scale;
+var timer;
+var buttons = new Array();
+var numberTex;
+var numbers = new Array();
 
+
+//Инициализация звуков
+var soundUp;
+var soundDown;
+function initMusic(){
+    soundUp = PIXI.sound.Sound.from("./up.mp3");  
+    soundDown = PIXI.sound.Sound.from("./down.mp3");  
+}
+
+//Добавляет новые значения полученные с сервера
+function newValueForBT(){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:3000/numbers.json', false);
+    xhr.send();
+    log(xhr.responseText);
+    var num = JSON.parse(xhr.responseText);
+    firstBT.children[0].destroy();
+    secondBT.children[0].destroy();
+    firstBT.value = num['first'];
+    secondBT.value = num['second'];
+    addNumber(firstBT);
+    addNumber(secondBT);
+}
+//Ticker для анимации уменьшения и увелечения кнопок
 var direct = -1;
 const ticker = new PIXI.ticker.Ticker();
 ticker.stop();
@@ -18,8 +47,11 @@ ticker.add((delta) =>{
             secondBT.scale.set(scale);
             firstBT.removeChild(firstBG);
             secondBT.removeChild(secondBG);
+            newValueForBT();
             direct = 1;
             ticker.start();
+            soundUp.play();
+
         }
         if(scale > 1)
         {
@@ -32,7 +64,7 @@ ticker.add((delta) =>{
             direct = -1;
         }
 });
-
+//Обработка нажатия на кнопку
 const onClick = event =>{
     if(firstBT == null)
     {
@@ -54,10 +86,12 @@ const onClick = event =>{
             secondBG = new PIXI.Sprite(glowTexture);
             secondBG.anchor.set(0.5);
             secondBT.addChild(secondBG);
-			if(firstBT.value == secondBT.value)
+
+            if(firstBT.value == secondBT.value)
             {
                 scale = 1;
                 ticker.start();
+                soundDown.play();
             }
             else
             {
@@ -68,48 +102,48 @@ const onClick = event =>{
             }
         }
 }
-
+//Добавление соответствующей цифры на кнопку
 function addNumber(button){
     if(button.value == 10)
-    {
-        var ten = new PIXI.Sprite() ;
-        var number = new PIXI.Sprite(numbers[1]);
-        number.scale.set(1.2);
-        number.anchor.set(0.5);
-        number.x = -10;
-        number.y = 3;
-        ten.addChild(number);
-        number = new PIXI.Sprite(numbers[0]);
-        number.anchor.set(0.5);
-        number.scale.set(1.2);
-        number.x = 10;
-        number.y = 0;
-        ten.addChild(number);
-        ten.anchor.set(0.5);
-        button.addChild(ten);
-    }
-    else
-    {
-        var number = new PIXI.Sprite(numbers[button.value]);
-        number.anchor.set(0.5);
-        number.scale.set(1.2);
-        button.addChild(number);
-    }
+            {
+                var ten = new PIXI.Sprite() ;
+                var number = new PIXI.Sprite(numbers[1]);
+                number.scale.set(1.2);
+                number.anchor.set(0.5);
+                number.x = -10;
+                number.y = 3;
+                ten.addChild(number);
+                number = new PIXI.Sprite(numbers[0]);
+                number.anchor.set(0.5);
+                number.scale.set(1.2);
+                number.x = 10;
+                number.y = 0;
+                ten.addChild(number);
+                ten.anchor.set(0.5);
+                button.addChild(ten);
+            }
+            else
+            {
+                var number = new PIXI.Sprite(numbers[button.value]);
+                number.anchor.set(0.5);
+                number.scale.set(1.2);
+                button.addChild(number);
+            }
 };
-
+//Добавление кнопки на игровое поле
 function addButton(){
     var button = new PIXI.Sprite.fromImage('chip.png');
     button.interactive = true;
     button.buttonMode = true;
+    button.value = Math.round(Math.random() * 10);
     button.anchor.set(0.5);
-	button.value = Math.round(Math.random() * 10);
-	addNumber(button);                    
-	button.on('pointerdown', onClick);
+    addNumber(button);                    
+    button.on('pointerdown', onClick);
     return button;
 };
-
+//Создание текстур для цифр
 function initNumbers(){
-    var numberTex = PIXI.Texture.fromImage("/BetFont/SafeDigits-hd.png");
+    numberTex = PIXI.Texture.fromImage("/BetFont/SafeDigits-hd.png");
     numbers.push(new PIXI.Texture(numberTex, new PIXI.Rectangle(0,57,20,20)));//0
     numbers.push(new PIXI.Texture(numberTex, new PIXI.Rectangle(115,0,13,20)));//1
     numbers.push(new PIXI.Texture(numberTex, new PIXI.Rectangle(0,39,20,18)));//2
@@ -121,22 +155,23 @@ function initNumbers(){
     numbers.push(new PIXI.Texture(numberTex, new PIXI.Rectangle(40,0,20,20)));//8
     numbers.push(new PIXI.Texture(numberTex, new PIXI.Rectangle(20,0,20,20)));//9
 };
-
+//Основна функция
 function init()
 {
-	initNumbers();
-	app = new PIXI.Application(window.innerWidth, 2000);
+    initMusic();
+    initNumbers();
+    app = new PIXI.Application(window.innerWidth, 2000);
     app.renderer = PIXI.autoDetectRenderer(816, 816,{antialiasing: false, transparent: false, resolution: 1});
     document.body.appendChild(app.view);
     app.renderer.backgroundColor = 0xffffff;
 
     var bgtexture = PIXI.Texture.fromImage("game_area.png");
-	bg = new PIXI.Sprite(bgtexture);
+    bg = new PIXI.Sprite(bgtexture);
     bg.position.x = 0;
     bg.position.y = 0;
     app.stage.addChild(bg);
-	
-	for(var row = 0; row < 4; row++)
+
+    for(var row = 0; row < 4; row++)
     {
         for(var col = 0; col < 4; col++)
         {
